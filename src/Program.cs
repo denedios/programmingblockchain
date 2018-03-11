698c690aba08e0836d2a93b4af5b75f1ab891b3e
chris denedios <cdenedios@therapeuticsmd.com>
2018-03-10 21:13:28 -0500

    code up to Bitcoin Transfer-Private Keys section of the book

--- src/Program.cs
+++ src/Program.cs
@@ -7,8 +7,11 @@ namespace programmingblockchain
     {
         static void Main(string[] args)
         {
-            var privateKey = new Key().GetWif(Network.Main);
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("\nBitcoin Address--------------\n");
+            
+            var privateKey = new Key().GetWif(Network.Main);
             Console.WriteLine($"Private key: {privateKey}");
 
             var publicKey = privateKey.PubKey;
@@ -22,6 +25,32 @@ namespace programmingblockchain
 
             var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
             Console.WriteLine($"Test net address: {testNetAddress}");
+
+            Console.WriteLine("\nScript Pub Key--------------\n");
+
+            var mainScriptPubKey = mainNetAddress.ScriptPubKey;
+            Console.WriteLine($"Main ScriptPubKey: {mainScriptPubKey}");
+
+            var testNetScriptPubKey = testNetAddress.ScriptPubKey;
+            Console.WriteLine($"Main ScriptPubKey: {testNetScriptPubKey}");
+
+            var paymentScript = publicKeyHash.ScriptPubKey;
+            var sameMainNetAddress = paymentScript.GetDestinationAddress(Network.Main);
+            Console.WriteLine(mainNetAddress == sameMainNetAddress); // True
+
+            var samePublicKeyHash = (KeyId) paymentScript.GetDestination();
+            Console.WriteLine(publicKeyHash == samePublicKeyHash); // true
+
+            var sameMainNetAddress2 = new BitcoinPubKeyAddress(samePublicKeyHash, Network.Main);
+            Console.WriteLine(mainNetAddress == sameMainNetAddress2);
+
+            Console.WriteLine("\nPrivate Key--------------\n");
+
+
+
+
+
+
 
 
             Console.WriteLine();
